fa64d82922f2323de7d8406b3231f3ff98be8232
sohanr <mizan.rahman66d@gmail.com>
2023-08-07 05:36:48 +0600

    transfer

--- contracts/Quinn.sol
+++ contracts/Quinn.sol
@@ -8,10 +8,32 @@ contract Quinn {
 
     uint256 public totalSupply;
 
+    // transfer event
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
     mapping(address => uint256) public balanceOf;
 
     constructor(uint256 _initialSupply) {
         totalSupply = _initialSupply; // 1 million
         balanceOf[msg.sender] = _initialSupply;
+    }
+
+    //transfer
+    //Exception if account does not have enough
+    //return a boolen
+    //transfer event
+    function transfer(
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);
+
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        // trigger the transfer event
+        emit Transfer(msg.sender, _to, _value);
+
+        return true;
     }
 }
