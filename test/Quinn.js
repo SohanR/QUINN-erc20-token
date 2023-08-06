102d7fe722adc63fe7f14ce507e406419615933f
sohanr <mizan.rahman66d@gmail.com>
2023-08-07 03:59:00 +0600

    test name

--- test/Quinn.js
+++ test/Quinn.js
@@ -1,17 +1,40 @@
 
 const Quinn = artifacts.require('Quinn');
+const common = require('../const')
 
 contract('Quinn', (accounts) => {
     let tokenInstance;
 
+    it('initializes the contract with the correct values', async () => {
+        tokenInstance = await Quinn.deployed();
+        assert.equal(
+            await tokenInstance.name(),
+            'Quinn',
+            'has the correct name'
+        );
+        assert.equal(
+            await tokenInstance.symbol(),
+            'QN',
+            'has the correct symbol'
+        )
+    })
+
+    // checking the total coin supply and balance of account
     it('allocates the initial supply upon deployment', async () => {
         tokenInstance = await Quinn.deployed();
         let totalSupply = await tokenInstance.totalSupply();
        
         assert.equal(
             totalSupply.toNumber(),
-            1000000,
-            'sets the total supply to 1000000'
+            common.TOTAL_SUPPLY,
+            'sets the total supply to ' + common.TOTAL_SUPPLY
+        );
+
+        let adminBalance = await tokenInstance.balanceOf(accounts[0]);
+        assert.equal(
+          adminBalance.toNumber(),
+          common.TOTAL_SUPPLY,
+          'it allocates the initial supply to the admin account'
         )
        
     })
