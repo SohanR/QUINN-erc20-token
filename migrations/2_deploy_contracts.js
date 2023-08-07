5a5ec042a58e659ce83138e85464f795cecf3b21
sohanr <mizan.rahman66d@gmail.com>
2023-08-08 05:28:22 +0600

    sale migratting

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,9 @@
 const Quinn = artifacts.require('Quinn');
+const QuinnSale = artifacts.require('QuinnSale');
 
 const common = require('../const');
 
 module.exports = (deployer) => {
     deployer.deploy(Quinn, common.TOTAL_SUPPLY);
+    deployer.deploy(QuinnSale, Quinn.address, common.TOKEN_PRICE );
 }
