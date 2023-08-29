9ba05a59a5eb91ba1dbe690d864f72cba2878db6
sohanr <mizan.rahman66d@gmail.com>
2023-08-30 04:59:44 +0600

    error fix

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,7 +3,13 @@ const QuinnSale = artifacts.require('QuinnSale');
 
 const common = require('../const');
 
-module.exports = (deployer) => {
-    deployer.deploy(Quinn, common.TOTAL_SUPPLY);
-    deployer.deploy(QuinnSale, Quinn.address, common.TOKEN_PRICE );
-}
+// module.exports = (deployer) => {
+//     deployer.deploy(Quinn, common.TOTAL_SUPPLY);
+//     deployer.deploy(QuinnSale, Quinn.address, common.TOKEN_PRICE );
+// }
+
+module.exports = async (deployer) => {
+    await deployer.deploy(Quinn, common.TOTAL_SUPPLY);
+    const quinnInstance = await Quinn.deployed(); 
+    await deployer.deploy(QuinnSale, quinnInstance.address, common.TOKEN_PRICE );
+};
